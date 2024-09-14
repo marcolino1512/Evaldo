6fc46e5f4f482d92533ad137d052dd433ae57a64
Marco Antonio Nascimento <137643410+marcolino1512@users.noreply.github.com>
2024-09-14 15:49:16 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,101 @@
+var navLinks=document.getElementById("navLinks");
+
+function showMenu(){
+    navLinks.style.right="0";
+}
+function hideMenu(){
+    navLinks.style.right="-200px";
+}
+
+  /* ========
+Debugger plugin, simple demo plugin to console.log some of callbacks
+======== */
+   function myPlugin({ swiper, extendParams, on }) {
+    extendParams({
+      debugger: false,
+    });
+
+    on('init', () => {
+      if (!swiper.params.debugger) return;
+      console.log('init');
+    });
+    on('click', (swiper, e) => {
+      if (!swiper.params.debugger) return;
+      console.log('click');
+    });
+    on('tap', (swiper, e) => {
+      if (!swiper.params.debugger) return;
+      console.log('tap');
+    });
+    on('doubleTap', (swiper, e) => {
+      if (!swiper.params.debugger) return;
+      console.log('doubleTap');
+    });
+    on('sliderMove', (swiper, e) => {
+      if (!swiper.params.debugger) return;
+      console.log('sliderMove');
+    });
+    on('slideChange', () => {
+      if (!swiper.params.debugger) return;
+      console.log(
+        'slideChange',
+        swiper.previousIndex,
+        '->',
+        swiper.activeIndex
+      );
+    });
+    on('slideChangeTransitionStart', () => {
+      if (!swiper.params.debugger) return;
+      console.log('slideChangeTransitionStart');
+    });
+    on('slideChangeTransitionEnd', () => {
+      if (!swiper.params.debugger) return;
+      console.log('slideChangeTransitionEnd');
+    });
+    on('transitionStart', () => {
+      if (!swiper.params.debugger) return;
+      console.log('transitionStart');
+    });
+    on('transitionEnd', () => {
+      if (!swiper.params.debugger) return;
+      console.log('transitionEnd');
+    });
+    on('fromEdge', () => {
+      if (!swiper.params.debugger) return;
+      console.log('fromEdge');
+    });
+    on('reachBeginning', () => {
+      if (!swiper.params.debugger) return;
+      console.log('reachBeginning');
+    });
+    on('reachEnd', () => {
+      if (!swiper.params.debugger) return;
+      console.log('reachEnd');
+    });
+  }
+
+
+
+  // Init Swiper
+  var swiper = new Swiper('.swiper', {
+    // Install Plugin To Swiper
+
+    loop:true,
+    centerdSlides:true,
+    autoplay:{
+        delay:5000,
+        disableOnInteraction:false,
+    },
+
+    modules: [myPlugin],
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    // Enable debugger
+    debugger: true,
+  });
